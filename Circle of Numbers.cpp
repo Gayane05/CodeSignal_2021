50449e21deb1968862a794fc50e3d61eebc517ae
Gayane Grigoryan <44817376+Gayane05@users.noreply.github.com>
2021-03-08 15:35:30 +0400

    Circle of Numbers.cpp

---
+++ Circle of Numbers.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+// CodeSignal
+//Consider integer numbers from 0 to n - 1 written down along the circle in such a way that the 
+//distance between any two neighboring numbers is equal(note that 0 and n - 1 are neighboring, too).
+//
+//Given n and firstNumber, find the number which is written in the radially opposite position to firstNumber.
+//
+//Example
+//
+//For n = 10 and firstNumber = 2, the output should be
+//circleOfNumbers(n, firstNumber) = 7.
+
+int circleOfNumbers(int n, int firstNumber)
+{
+	return firstNumber >= n / 2 ? firstNumber - (n / 2) : firstNumber + (n / 2);
+}
+
+// NBME
+//int circleOfNumbers(int n, int firstNumber) {
+//	return (firstNumber + n / 2) % n;
+//}
+
+int main()
+{
+	circleOfNumbers(18, 6);
+
+	return 0;
+}
